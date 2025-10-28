5552ccdcd788de2074d601c60f82f85cc555c59e
aglaura <aglaura@msn.com>
2025-10-28 14:27:08 +0800

    fix(config): manifest ..

--- next.config.js
+++ next.config.js
@@ -65,14 +65,18 @@ const nextConfig = {
   },
 };
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable:
-    process.env.NODE_ENV === 'development' ||
-    process.env.NEXT_DISABLE_PWA === '1' ||
-    process.env.NEXT_DISABLE_PWA === 'true',
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA(nextConfig);
+if (
+  process.env.NODE_ENV === 'development' ||
+  process.env.NEXT_DISABLE_PWA === '1' ||
+  process.env.NEXT_DISABLE_PWA === 'true'
+) {
+  module.exports = nextConfig;
+} else {
+  const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: false,
+    register: true,
+    skipWaiting: true,
+  });
+  module.exports = withPWA(nextConfig);
+}
