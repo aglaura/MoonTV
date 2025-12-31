7e7ec9ca093f82d8e40f1d4ffecb15548b10caf6
aglaura <aglaura@msn.com>
2025-12-31 09:04:05 +0800

    auto: update on 2025-12-31 09:04:05

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2158,7 +2158,6 @@ function PlayPageClient() {
         },
         // @ts-expect-error Artplayer options accept this
         click: false,
-        // @ts-expect-error Artplayer options accept this
         dblclick: false,
         // HLS 支持配置
         customType: {
